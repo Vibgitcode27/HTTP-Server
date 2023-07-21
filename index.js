2ec31885e2697fac4e47f32ee08272f6e34390a5
Vibhor <vibgitcode27@gmail.com>
2023-07-21 23:09:16 +0530

    post request through query , headers and body. Status codes , sending HTML file in response

--- index.js
+++ index.js
@@ -28,6 +28,7 @@
 // app.listen(port , listener)
 
 let express = require(`express`)
+let bodyParser = require(`body-parser`)  // This liberary is used to fetch library
 let app = express()
 let port = 3000
 
@@ -40,21 +41,20 @@ calSum = (num) =>
     return count
 }
 
-executor = (req , res) =>
-{
-    let count = req.query.count // the request we are giving to the host is http://localhost:3000/vibhor?count=10
-    res.send(`The sum is ${calSum(count)}`)
-}
+// MIDDLEWares 
 
-app.get(`/vibhor`, executor)
+app.use(bodyParser.json())  // This also a middle ware
 
- post = (req ,res) =>
- {
-    res.send(`This is a successful post request`)
- }
-app.post("/post" , post)
+// middleWare = (req , res , next) =>
+// {
+//     console.log("This is a middleWare " + req.headers.count)
+//     next()
+// }
 
-// This api request runs code foreven (until we close port) and listens requests
+// app.use(middleWare)
+
+
+// This api request runs code forever (until we close port) and listens requests
 
 listener = () =>
 {
@@ -62,3 +62,43 @@ listener = () =>
 }
 
 app.listen(port , listener)
+
+// GET request
+
+executor = (req , res) =>
+{
+    let count = req.query.count // the request we are giving to the host is http://localhost:3000/vibhor?count=10
+    res.status(201).send(`The sum is ${calSum(count)}`) // You can also add status codes
+}
+
+app.get(`/vibhor`, executor)
+
+// POST Request
+
+ post = (req ,res) =>
+ {
+    if (req.body.count < 30000){
+        console.log(req.body)
+        // let count = req.query.count    //***** Counter from query
+        // let count = req.headers.count  //***** Count from headers
+        let count = req.body.count        //***** count form body  
+        
+                let retJSON = {
+                    Sum : calSum(count)
+                }
+        res.status(202).send(retJSON)
+    } 
+    else {
+        res.status(406).send("Ask is big")
+    }
+ }
+app.post("/post" , post)
+
+// This is How you send Html FIle
+
+post_html = (req ,res) =>
+{
+    res.sendFile(__dirname + '/index.html')
+}
+
+app.get(`/get/html` , post_html)
